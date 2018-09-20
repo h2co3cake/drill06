50900d9bb66e03fc5b471b5ab8ecf636bb9e1417
Leejuwon(school) <bbit456@naver.com>
2018-09-20 18:36:44 +0900

    drill06_02
    hand  이미지와 마우스 클릭할 때 캐릭터가 바로 이동하게 설정

--- drill06.py
+++ drill06.py
@@ -2,15 +2,14 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-
 def handle_events():
-    global  running
+    global running
     global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
+        elif event.type == SDL_MOUSEBUTTONDOWN:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
@@ -18,6 +17,7 @@ def handle_events():
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
