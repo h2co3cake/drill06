48ae998f5ac777ccac69c240829d9f49d4709bb0
Leejuwon(school) <bbit456@naver.com>
2018-09-20 19:12:05 +0900

    drill06_06
    핸드의 x좌표와 y좌표 캐릭터보다 높은 곳에 있을때 달리기모션을 하면서 이동하도록 설정1

--- drill06.py
+++ drill06.py
@@ -7,16 +7,20 @@ def handle_events():
     global x, y
     global hand_x, hand_y
     global LR
+
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+
         elif event.type == SDL_MOUSEMOTION:
             hand_x, hand_y = event.x + 20, KPU_HEIGHT - 1 - event.y - 30
+
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            if x <= event.x:
+            if x < event.x:
                 x, y, LR = event.x, KPU_HEIGHT - 1 - event.y, 1
+
             elif event.x < x:
                 x, y, LR = event.x, KPU_HEIGHT - 1 - event.y, 0
 
@@ -31,6 +35,7 @@ hand = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x1, y1 = KPU_WIDTH // 2, KPU_HEIGHT // 2
 hand_x, hand_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 LR = 1
@@ -40,11 +45,17 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     hand.draw_now(hand_x, hand_y)
-    character.clip_draw(frame * 100, 100 * LR, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * LR, 100, 100, x1, y1)
     update_canvas()
+
+    handle_events()
     frame = (frame + 1) % 8
 
+    if x1 < x and y1 < y:
+        x1 += 10
+        y1 += 10
+ 
+
     delay(0.05)
-    handle_events()
 
 close_canvas()
